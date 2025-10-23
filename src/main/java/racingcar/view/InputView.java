d6420b0adba98cf481ea387f8747e1e1d120da7c
A7024E <rudqls6671@naver.com>
2025-10-23 13:23:59 +0900

    refactor: 입력 값에 대한 검증 메서드 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,9 +7,13 @@ public class InputView {
 
     public String inputCarName() {
         String input = Console.readLine();
+        validateInput(input);
+        return Console.readLine();
+    }
+
+    private static void validateInput(String input) {
         InputValidation.validateCarName(input);
         InputValidation.validateCarNameBlank(input);
-        return Console.readLine();
     }
 
 }
