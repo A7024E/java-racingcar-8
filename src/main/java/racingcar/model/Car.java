d1f2e84774e00c6b75b6ff3024bf26c4ffb34cba
A7024E <rudqls6671@naver.com>
2025-10-23 13:40:20 +0900

    feat: 자동차 이름이 5글자 초과시 예외처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -5,6 +5,7 @@ public class Car {
     private final int position;
 
     private Car(String name) {
+        validateCar(name);
         this.name = name;
         this.position = 0;
     }
@@ -19,5 +20,11 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    private void validateCar(String name){
+        if(name.length() > 5){
+            throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다");
+        }
     }
 }
