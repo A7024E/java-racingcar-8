6f33333c1d1650366c381aa51300f4195ecc7797
A7024E <rudqls6671@naver.com>
2025-10-23 15:14:31 +0900

    test: RacingCount 테스트 - RacingCount 객체가 정상적으로 생성되는지 테스트 - 시도 횟수 1미만에 대한 예외 테스트

---
+++ src/test/java/racingcar/model/RacingCountTest.java
@@ -0,0 +1,32 @@
+package racingcar.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingCountTest {
+    @DisplayName("시도 횟수를 받아 RacingCount 객체를 생성한다.")
+    @Test
+    void createRacingCount() {
+        // given
+        int count = 1;
+        // when
+        RacingCount racingCount = RacingCount.from(count);
+        // then
+        assertThat(racingCount).isEqualTo(RacingCount.from(1));
+    }
+
+    @DisplayName("시도 횟수가 1보다 작은경우 예외를 발생한다")
+    @Test
+    void racingCountIsLessThanOne() {
+        // given
+        int count = 0;
+        //when// then
+        assertThatThrownBy(() -> RacingCount.from(count))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
