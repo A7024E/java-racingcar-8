8f0f7ac14a72a76cac4f6e3dc92e51b1fd478682
A7024E <rudqls6671@naver.com>
2025-10-23 15:23:07 +0900

    feat: 경주에 참가할 자동차들을 관리하는 일급컬렉션 Cars 추가

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars of(List<Car> cars) {
+        return new Cars(cars);
+    }
+
+
+}
