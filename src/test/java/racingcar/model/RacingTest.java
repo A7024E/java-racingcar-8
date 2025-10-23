22a4f087213961593a3c7fad36abbda916aa189c
A7024E <rudqls6671@naver.com>
2025-10-23 16:43:33 +0900

    test: 경주 진행 로직에 대한 테스트 추가
    - raceOnce() 호출 시 자동차들이 랜덤 값 조건에 따라 올바르게 이동하는지 검증
    - StubRandomNumberGenerator를 이용해 랜덤 값을 고정하여 예측 가능한 테스트 구현

--- src/test/java/racingcar/model/RacingTest.java
+++ src/test/java/racingcar/model/RacingTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.utils.RandomNumberGenerator;
+import racingcar.utils.StubRandomNumberGenerator;
 
 class RacingTest {
     @DisplayName("자동차 객체와 레이싱 카운트 객체 를 이용하여 경주생성 테스트")
@@ -21,9 +22,28 @@ class RacingTest {
 
         RacingCount racingCount = RacingCount.from(1);
         // when
-        Racing racing = Racing.of(cars, racingCount);
+        Racing racing = Racing.from(cars);
         // then
-        assertThat(racing).isEqualTo(Racing.of(cars, racingCount));
+        assertThat(racing).isEqualTo(Racing.from(cars));
+    }
+
+    @DisplayName("경주가 잘 진행되는지 테스트한다")
+    @Test
+    void runRacing() {
+        // given
+        Car pobi = Car.from("pobi", new StubRandomNumberGenerator(4));
+        Car woni = Car.from("woni", new StubRandomNumberGenerator(3));
+        Car jun = Car.from("jun", new StubRandomNumberGenerator(2));
+
+        Cars cars = Cars.from(List.of(pobi,woni,jun));
+        Racing racing = Racing.from(cars);
+        // when
+        racing.raceOnce();
+        // then
+        assertThat(pobi.getPosition()).isEqualTo(1);
+        assertThat(woni.getPosition()).isEqualTo(0);
+        assertThat(jun.getPosition()).isEqualTo(0);
+
     }
 
 
