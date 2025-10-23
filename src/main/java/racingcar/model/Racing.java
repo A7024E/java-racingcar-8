80f4bd56649b3014d3e567e9ee4dcca6f11c620f
A7024E <rudqls6671@naver.com>
2025-10-23 16:26:47 +0900

    refactor: Racing 객체의 racingCount 필드 제거 - 경주 횟수의 책임은 RacingCount에서 가진다 - 경주의 흐름의 책임은 Controller에서 가진다. - 따라서 Racing은 경주 횟수에는 관여하지 않고, 경주를 진행할 책임만 가지면 된다.

--- src/main/java/racingcar/model/Racing.java
+++ src/main/java/racingcar/model/Racing.java
@@ -4,15 +4,13 @@ import java.util.Objects;
 
 public class Racing {
     private final Cars cars;
-    private final RacingCount racingCount;
 
-    private Racing(Cars cars, RacingCount racingCount) {
+    private Racing(Cars cars) {
         this.cars = cars;
-        this.racingCount = racingCount;
     }
 
-    public static Racing of(Cars cars, RacingCount racingCount) {
-        return new Racing(cars, racingCount);
+    public static Racing of(Cars cars) {
+        return new Racing(cars);
     }
 
     @Override
@@ -24,11 +22,11 @@ public class Racing {
             return false;
         }
         Racing racing = (Racing) o;
-        return Objects.equals(cars, racing.cars) && Objects.equals(racingCount, racing.racingCount);
+        return Objects.equals(cars, racing.cars);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(cars, racingCount);
+        return Objects.hashCode(cars);
     }
 }
