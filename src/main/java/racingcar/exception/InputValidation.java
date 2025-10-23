cb8799241e9e6466a08379d7ee04bdf3b87966f0
A7024E <rudqls6671@naver.com>
2025-10-23 13:59:09 +0900

    refactor: 입력값 유효성 검사 메서드 반환 타입 변경
    - 입력값 유효성 검사를 boolean 타입으로 반환하도록 수정
    - 동일한 검증 로직 내에서 참/거짓만 판단하여 상황별 예외 메시지를 분리해 처리하도록 개선
    - 상속을 방지하기 위해 final 클래스 선언으로 변경

--- src/main/java/racingcar/exception/InputValidation.java
+++ src/main/java/racingcar/exception/InputValidation.java
@@ -1,26 +1,16 @@
 package racingcar.exception;
 
-public class InputValidation {
+public final class InputValidation {
 
-    public static void validateCarName(String carName){
-        if(isCarNameNull(carName) || carName.isEmpty()){
-            throw new IllegalArgumentException("경주할 자동차 이름을 입력해야합니다");
-        }
+    private InputValidation() {
+
     }
 
-    public static void validateCarNameBlank(String carName){
-        if(isBlankContains(carName)){
-            throw new IllegalArgumentException("공백이 포함할 수 없습니다");
-        }
+    public static boolean isNullOrEmpty(String carName) {
+        return carName == null || carName.isEmpty();
     }
 
-    private static boolean isBlankContains(String carName) {
+    public static boolean isBlankContains(String carName) {
         return carName.contains(" ");
     }
-
-    private static boolean isCarNameNull(String carName) {
-        return carName == null;
-    }
-
-
 }
