ed32123928408b8963ef159e62c17624ba148785
A7024E <rudqls6671@naver.com>
2025-10-24 21:40:07 +0900

    test: 경주결과 반환 테스트 - 경주를 실행 했을때 정상적으로 결과가 집계되는지 테스트한다.

--- src/test/java/racingcar/model/RacingTest.java
+++ src/test/java/racingcar/model/RacingTest.java
@@ -1,11 +1,13 @@
 package racingcar.model;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.dto.CarResultDto;
 import racingcar.utils.RandomNumberGenerator;
 import racingcar.utils.StubRandomNumberGenerator;
 
@@ -43,6 +45,32 @@ class RacingTest {
         assertThat(pobi.getPosition()).isEqualTo(1);
         assertThat(woni.getPosition()).isEqualTo(0);
         assertThat(jun.getPosition()).isEqualTo(0);
+
+    }
+
+    @DisplayName("경주 결과를 반환한다")
+    @Test
+    void racingResult(){
+        // given
+        Car pobi = Car.from("pobi", new StubRandomNumberGenerator(4));
+        Car woni = Car.from("woni", new StubRandomNumberGenerator(3));
+        Car jun = Car.from("jun", new StubRandomNumberGenerator(1));
+
+        Cars cars = Cars.from(List.of(pobi,woni,jun));
+        Racing racing = Racing.from(cars);
+        racing.raceOnce();
+
+        // when
+        List<CarResultDto> racingResults = racing.racingResult();
+        // then
+        assertThat(racingResults).containsExactly(
+                CarResultDto.of("pobi",1),
+                CarResultDto.of("woni",0),
+                CarResultDto.of("jun",0)
+        );
+
+
+
 
     }
 
