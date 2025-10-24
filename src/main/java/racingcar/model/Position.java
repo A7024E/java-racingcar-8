6ccf33d1cfe2e8231a7f63076e6f629f45a43361
A7024E <rudqls6671@naver.com>
2025-10-25 05:35:05 +0900

    feat: name, position equals & hashcode 재정의

--- src/main/java/racingcar/model/Position.java
+++ src/main/java/racingcar/model/Position.java
@@ -13,4 +13,20 @@ public class Position {
         return new Position(position);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return Position == position.Position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(Position);
+    }
 }
