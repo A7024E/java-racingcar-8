0eddff6290a81f2e726fe800468cbdf8da9cfa99
A7024E <rudqls6671@naver.com>
2025-10-24 23:58:36 +0900

    refactor: 객체안에 객체 참조하는 if문 수정

--- src/main/java/racingcar/exception/CarValidation.java
+++ src/main/java/racingcar/exception/CarValidation.java
@@ -7,20 +7,19 @@ public final class CarValidation {
     }
 
     public static void validate(String input) {
-        if (CarValidation.isNullOrEmpty(input)) {
+        if (isNullOrEmpty(input)) {
             throw new IllegalArgumentException("자동차 이름을 입력해야 합니다.");
         }
-        if (CarValidation.isBlankContains(input)) {
+        if (isBlankContains(input)) {
             throw new IllegalArgumentException("공백을 입력할 수 없습니다.");
         }
-
         if (isCarNameRange(input)) {
             throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다");
         }
     }
 
     private static boolean isNullOrEmpty(String carName) {
-        return carName == null || carName.isEmpty() ;
+        return carName == null || carName.isEmpty();
     }
 
     private static boolean isBlankContains(String carName) {
