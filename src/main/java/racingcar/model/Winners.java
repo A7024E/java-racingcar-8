a5065f9969fe66019ed493f79f5d939a60358dda
A7024E <rudqls6671@naver.com>
2025-10-25 02:20:37 +0900

    feat: winners 의 equals & hashcode 재정의

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Winners {
     private final List<Winner> winners;
@@ -13,5 +14,20 @@ public class Winners {
         return new Winners(winners);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        };
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Winners winners1 = (Winners) o;
+        return Objects.equals(winners, winners1.winners);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(winners);
+    }
 }
