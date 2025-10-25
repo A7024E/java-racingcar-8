3c87397b48f35432f018e6cfbbee486b09ecf6bc
A7024E <rudqls6671@naver.com>
2025-10-26 02:38:38 +0900

    refactor: 컨버터에 역할에 맞게 이름 수정
    - generateCars 는 Cars 객체의 역할 네이밍이 너무 강해 수정

--- src/main/java/racingcar/utils/Converter.java
+++ src/main/java/racingcar/utils/Converter.java
@@ -1,14 +1,22 @@
 package racingcar.utils;
 
 import java.util.List;
+import racingcar.exception.ErrorMessages;
 
 public class Converter {
     private Converter() {
 
     }
-    
 
-    public static List<String> generateCarNames(String[] carNames){
+    public static int toInt(String racingCount) {
+        try {
+            return Integer.parseInt(racingCount);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_RACING_COUNT_INPUT.message());
+        }
+    }
+
+    public static List<String> toList(String[] carNames){
         return List.of(carNames);
     }
 }
