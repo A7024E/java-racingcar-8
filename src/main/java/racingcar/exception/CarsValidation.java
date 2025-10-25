707f71fcd0dbccb76ae6d3182cc4f99168eaa670
A7024E <rudqls6671@naver.com>
2025-10-26 02:07:53 +0900

    feat: Cars 의 유효성을 모아둔 객체 생성

---
+++ src/main/java/racingcar/exception/CarsValidation.java
@@ -0,0 +1,19 @@
+package racingcar.exception;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import racingcar.model.Car;
+
+public class CarsValidation {
+    public static void validate(List<Car> cars) {
+        validateCars(cars);
+    }
+
+    private static void validateCars(List<Car> cars) {
+        Set<Car> nonDuplicateCars = new HashSet<>(cars);
+        if (nonDuplicateCars.size() != cars.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복이 될 수 없습니다");
+        }
+    }
+}
