699cd8cf387f27dff7fd6e7c1418a1de4514e893
A7024E <rudqls6671@naver.com>
2025-10-26 02:41:29 +0900

    test: 시도 횟수에 정수 외의 문자가 들어갔을때 예외발생

--- src/test/java/racingcar/utils/ConverterTest.java
+++ src/test/java/racingcar/utils/ConverterTest.java
@@ -1,12 +1,15 @@
 package racingcar.utils;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.model.Cars;
 
 class ConverterTest {
 
@@ -23,6 +26,16 @@ class ConverterTest {
         List<String> cars = Converter.toList(splitCarNames);
         //then
         assertThat(cars).hasSize(carNameSize);
+    }
+
+    @DisplayName("시도 횟수에 정수 외의 문자가 들어갔을때 예외발생")
+    @Test
+    void invalidRacingCountInput(){
+        // given
+        String input = "a123";
+        // when// then
+        assertThatThrownBy(() -> Converter.toInt(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
