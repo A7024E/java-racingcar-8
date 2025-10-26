818485637775dc9be84d44dad80c57444e136f8a
A7024E <rudqls6671@naver.com>
2025-10-27 00:42:42 +0900

    feat: 연속된 구분자를 입력할 경우 예외발생 메시지 생성

--- src/main/java/racingcar/exception/ErrorMessages.java
+++ src/main/java/racingcar/exception/ErrorMessages.java
@@ -7,7 +7,8 @@ public enum ErrorMessages {
     DUPLICATE_CAR_NAME("자동차 이름은 중복이 될 수 없습니다."),
     NEGATIVE_POSITION("포지션은 음수가 될 수 없습니다."),
     INVALID_RACING_COUNT("시도 횟수는 최소 1 이상이여야 합니다."),
-    INVALID_RACING_COUNT_INPUT("시도 횟수는 정수로 입력해야 합니다.");
+    INVALID_RACING_COUNT_INPUT("시도 횟수는 정수로 입력해야 합니다."),
+    CONTINUOUS_DELIMITER_ERROR_MESSAGE("연속된 구분자를 입력할 수 없습니다.");
 
 
     private final String message;
