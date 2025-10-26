7eca3b7324db8aec758e395650841ea53351e947
A7024E <rudqls6671@naver.com>
2025-10-27 00:42:52 +0900

    feat: 연속된 구분자를 입력할 경우 예외발생 기능

--- src/main/java/racingcar/utils/Converter.java
+++ src/main/java/racingcar/utils/Converter.java
@@ -9,6 +9,11 @@ public class Converter {
 
     }
 
+    public static List<String> toList(String[] carNames){
+        validateDelimiter(carNames);
+        return List.of(carNames);
+    }
+
     public static int toInt(String racingCount) {
         try {
             return Integer.parseInt(racingCount);
@@ -17,7 +22,15 @@ public class Converter {
         }
     }
 
-    public static List<String> toList(String[] carNames){
-        return List.of(carNames);
+    public static void validateDelimiter(String[] splitCarNames) {
+        for (String splitCarName : splitCarNames) {
+            if (isNullOrEmpty(splitCarName)) {
+                throw new IllegalArgumentException(ErrorMessages.CONTINUOUS_DELIMITER_ERROR_MESSAGE.message());
+            }
+        }
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 }
